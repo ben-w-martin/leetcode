9e3fe1e4cad0644cdb2d9ad64d074f9a836a0e4e
Benjamin Martin <ben.w.martin17@gmail.com>
2024-06-09 10:20:54 -0400

    fix: rm comments

--- typescript/max-avg-subarray.ts
+++ typescript/max-avg-subarray.ts
@@ -28,29 +28,3 @@ function findMaxAverage(nums: number[], k: number): number {
 const arr: number[] = [6,8,6,8,0,4,1,2,9,9]
 
 console.log(findMaxAverage(arr, 2))
-
-//#region OLD ATTEMPT
-// function findMaxAverage(nums: number[], k: number): number {
-    //     let kLengthArr: number[] = nums.slice(0, k);
-    //     let sum: number = kLengthArr.reduce((acc, num) => acc + num);
-    //     let avg: number = sum / k
-//     let max: number = avg
-//     console.log("sum: ", sum);
-//     console.log("avg: ", avg);
-
-//     for (let i: number = 1; i < nums.length; i++) {
-//         if (!nums[i + 1]) break;
-
-//         sum -= nums[i - 1];
-//         sum += nums[i + 1]
-//         console.log("sum: ", sum);
-        
-//         let avg: number = sum / k;
-//         console.log("avg: ", avg);
-        
-//         max = max > avg ? max : avg;
-//     } 
-
-//     return max
-// };
-//#endregion
